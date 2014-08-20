a53627e2fb068a4188da16f2b06bc349a2e05f76
Lucas Sinclair <lucas.sinclair@me.com>
2014-08-21 01:29:31 +0200

    Update the setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
       name             = 'illumitag',
-      version          = '1.0.1',
+      version          = '1.0.2',
       description      = 'Pipeline for 16S Illumina paired-end sequencing',
-      long_description = open('README.txt').read(),
+      long_description = open('README.md').read(),
       license          = 'MIT',
       url              = 'http://github.com/limno/illumitag/',
       author           = 'Lucas Sinclair',
       author_email     = 'lucas.sinclair@me.com',
       classifiers      = ['Topic :: Scientific/Engineering :: Bio-Informatics'],
       packages         = ['illumitag'],
-      requires         = ['biopython', 'matplotlib', 'threadpool', 'sh', 'patsy', 'pandas', 'statsmodels', 'rpy2', 'scikit-learn', 'rpy2', 'brewer2mpl', 'regex', 'ftputil', 'names', 'shell_command', 'pystache', 'tabulate', 'tqdm', 'scikit-bio==0.1.4'],
+      requires         = ['plumbing', 'fasta', 'sh', 'biopython', 'matplotlib', 'threadpool', 'patsy', 'pandas', 'statsmodels', 'rpy2', 'scikit-learn', 'rpy2', 'brewer2mpl', 'regex', 'ftputil', 'names', 'shell_command', 'pystache', 'tabulate', 'tqdm', 'scikit-bio==0.1.4'],
 )
